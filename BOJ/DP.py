6263cafad9326cb245c1707ebd255b8b33da73d8
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-07-13 15:04:41 +0900

    docs: Edit DP.py to include solution about #11726

--- BOJ/DP.py
+++ BOJ/DP.py
@@ -15,3 +15,19 @@ for i in range(2, n + 1):
         solution[i] = solution[i // 3] + 1
         
 print(solution[n])
+
+
+
+#11726
+
+n = int(input())
+
+solution = [0 for _ in range(1001)]
+
+solution[1] = 1
+solution[2] = 2
+for i in range(3, n + 1):
+    
+    solution[i] = (solution[i - 1] + solution[i - 2] ) % 10007
+    
+print(solution[n] % 10007)
