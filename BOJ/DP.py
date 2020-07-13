69d878ef09e52d03f7b45fe9e044ec540f31a8f8
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-07-13 15:32:33 +0900

    docs: Edit DP.py to include code about #9095

--- BOJ/DP.py
+++ BOJ/DP.py
@@ -47,3 +47,20 @@ for i in range(3, n + 1):
     solution[i] = (solution[i - 1] + 2 * solution[i - 2] ) % 10007
     
 print(solution[n] % 10007)
+
+
+
+#9095
+
+n = int(input())
+
+solution = [0 for _ in range(n + 1)]
+
+solution[1] = 1
+solution[2] = 2
+solution[3] = 4
+for i in range(4, n + 1):
+    
+    solution[i] = solution[i - 3] + solution[i - 2] + solution[i - 1]
+
+print(solution[n])
