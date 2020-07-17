618d79709229ce28fa9514ffb1a72743dd06277d
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-07-17 22:03:58 +0900

    docs: Edit DP.py to include solution about #11055

--- BOJ/DP.py
+++ BOJ/DP.py
@@ -181,3 +181,19 @@ for i in range(n):
     solution[i] = min_ + 1
     
 print(max(solution))
+
+
+
+#11055
+
+n = int(input())
+numbers = list(map(int, input().split()))
+
+solution = list(map(int, numbers))
+
+for i in range(n):
+    for j in range(i):    
+        if numbers[i] > numbers[j]:
+            solution[i] = max(solution[i], solution[j] + numbers[i])
+              
+print(max(solution))
