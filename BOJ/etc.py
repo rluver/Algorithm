d77378140409c2baf4d9d1817a53614e2e548e43
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-07-21 18:43:34 +0900

    docs: Edit DP.py to include solution about #11651

--- BOJ/etc.py
+++ BOJ/etc.py
@@ -21,3 +21,17 @@ solution.sort(key = lambda x: (x[0], x[1]))
 
 for coord in solution:
     print(coord[0], coord[1])
+
+    
+    
+# 11651
+
+n = int(input())
+
+solution = [list(map(int, input().split())) for _ in range(n)]
+
+solution.sort(key = lambda x: (x[1], x[0]))
+
+for coord in solution:
+    print(coord[0], coord[1])
+    
