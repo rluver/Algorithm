eb720ed381cac615c9f9e11982336406d263804f
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-08-06 23:23:38 +0900

    docs: Edit Heap.py to include solution about #11279

--- BOJ/Heap.py
+++ BOJ/Heap.py
@@ -26,3 +26,35 @@ class MinHeap:
 
 minheap = MinHeap(int(input()))
 minheap.getMinHeap()
+
+
+
+# 11279
+
+import sys
+import heapq
+
+
+class MaxHeap:
+    
+    def __init__(self, N):
+        self.N = N
+        self.heap = []
+    
+    def insertNum(self):
+        self.num = int(sys.stdin.readline())
+        
+        return self.num
+        
+    def getMaxHeap(self):        
+        for _ in range(self.N):
+            if self.insertNum() != 0:
+                heapq.heappush(self.heap, -self.num)
+            else:
+                if self.heap:
+                    print(-1 * heapq.heappop(self.heap))
+                else:
+                    print(0)
+
+minheap = MaxHeap(int(input()))
+minheap.getMaxHeap()
