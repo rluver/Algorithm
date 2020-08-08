6106732f5e60dcd46d53e79a20f5e0cdd80b1213
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-08-08 22:37:18 +0900

    docs: Edit Heap.py to include solution about #1766

--- BOJ/Heap.py
+++ BOJ/Heap.py
@@ -91,3 +91,55 @@ class absHeap:
 
 absheap = absHeap(int(input()))
 absheap.getabsHeap()
+
+
+
+# 1766
+
+import heapq
+
+class Workbook:
+
+    def __init__(self, N, M):        
+        self.N = N
+        self.M = M
+
+        self.numLists = [[] for _ in range(self.N + 1)]
+        self.inDegree = [0 for _ in range(self.N + 1)]
+
+        self.heap = []
+        self.result = []
+
+    def GraphInformation(self):
+        for i in range(self.M):
+            x, y = map(int, input().split())
+            self.numLists[x].append(y)
+            self.inDegree[y] += 1
+
+    def makeFirstHeap(self):
+        for i in range(1, self.N + 1):
+            if self.inDegree[i] == 0:
+                heapq.heappush(self.heap, i)
+
+    def makeTopologicalSort(self):
+        while self.heap:
+            temp = heapq.heappop(self.heap)
+            self.result.append(temp)
+
+            for i in self.numLists[temp]:
+                self.inDegree[i] -= 1
+
+                if self.inDegree[i] == 0:
+                    heapq.heappush(self.heap, i)
+    
+    def showResult(self):
+        for i in self.result:
+            print(i, end = ' ')
+
+                    
+N, M = map(int, input(). split()) 
+workbook = Workbook(N, M) 
+workbook.GraphInformation() 
+workbook.makeFirstHeap() 
+workbook.makeTopologicalSort()
+workbook.showResult()
