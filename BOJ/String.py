d23237cceee32dab12e27f37575b1d7473044e21
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-08-14 17:22:21 +0900

    docs: Edit String.py to include solution about #1152

--- BOJ/String.py
+++ BOJ/String.py
@@ -2,3 +2,22 @@
 
 n = input()
 print(ord(n))
+
+
+
+# 1152
+
+class getWordCount():
+    
+    def __init__(self, input):
+        self.input = input.strip()
+        
+    def CalWordCount(self):
+        
+        split_string = self.input.split(' ')
+        num = len(split_string)
+        print(num)
+        
+    
+getwordcount = getWordCount('Teullinika Teullyeotzi ')
+getwordcount.CalWordCount()
