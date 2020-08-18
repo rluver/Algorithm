7c5d861c0a5e4b5b752fa308925bfced73c5a473
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-08-18 20:56:14 +0900

    docs: Edit Brute-Force Search.py to include solution about #2309

--- BOJ/Brute-Force Search.py
+++ BOJ/Brute-Force Search.py
@@ -1,3 +1,42 @@
+# 2309
+
+import sys
+
+
+class Dwarf():
+    
+    def __init__(self):
+        self.Lists = []
+        self.First = 0
+        self.Second = 0
+    
+    def insertHeight(self):
+        for _ in range(9):
+            self.Lists.append(int(sys.stdin.readline()))
+            
+        self.sum = sum(self.Lists)
+        
+    def getAnswer(self):
+        for i in range(8):
+            for j in range(i + 1, 9):
+                if self.sum - (self.Lists[i] + self.Lists[j]) == 100:
+                    self.First = self.Lists[i]
+                    self.Second = self.Lists[j]
+                    
+        self.Lists.remove(self.First)
+        self.Lists.remove(self.Second)
+        self.Lists.sort()
+        
+        for i in self.Lists:
+            print(i)
+ 
+
+dwarf = Dwarf()
+dwarf.insertHeight()
+dwarf.getAnswer()
+
+
+
 # 1476
 
 class YearCalculation:
